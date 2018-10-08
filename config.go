cec8d667a6e3522e614fb627d6780ca4e61742d3
Jack.Ju <jack.ju@itiky.com>
2018-10-08 09:56:49 +0800

    Update config.go

--- config.go
+++ config.go
@@ -2,15 +2,59 @@ package gbatis
 
 import (
 	"encoding/xml"
+	"os"
+	"log"
+	"io/ioutil"
 )
 
 type configuration struct {
-	XMLName xml.Name `xml:"configuration"`
+	XMLName xml.Name     `xml:"configuration"`
+	Envs    environments `xml:"environments"`
 }
 
 type environments struct {
+	Default string        `xml:"default,attr"`
+	Envs    []environment `xml:"environment"`
 }
 
 type environment struct {
-	ID string `xml:"id,attr"`
+	ID         string     `xml:"id,attr"`
+	DataSource dataSource `xml:"dataSource"`
+}
+
+type dataSource struct {
+	Type       string     `xml:"type,attr"`
+	Properties []property `xml:"property"`
+}
+
+type property struct {
+	Name  string `xml:"name,attr"`
+	Value string `xml:"value,attr"`
+}
+
+func loadConfig(configFile string) (*configuration, error) {
+
+	file, err := os.Open(configFile)
+	if err != nil {
+		log.Printf("error: %v", err)
+		return nil, err
+	}
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Printf("error: %v", err)
+		return nil, err
+	}
+
+	c := configuration{}
+	err = xml.Unmarshal(data, &c)
+	if err != nil {
+		log.Printf("error: %v", err)
+		return nil, err
+	}
+
+	log.Println(c)
+
+	return &c, nil
 }
