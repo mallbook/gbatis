6cbed20d945d1ea3059fcdb128c1230050da106a
Jack.Ju <jack.ju@itiky.com>
2018-10-16 20:23:48 +0800

    Create dbmgr.go

---
+++ dbmgr.go
@@ -0,0 +1,98 @@
+package gbatis
+
+import (
+	"database/sql"
+	"fmt"
+	"sync"
+	"log"
+)
+
+var (
+	_dbmgr *dbMgr
+	_once  sync.Once
+)
+
+func getDBMgrInstance() *dbMgr {
+	_once.Do(func() {
+		_dbmgr = &dbMgr{
+			dbs: make(map[string]*sql.DB),
+		}
+	})
+	return _dbmgr
+}
+
+type dbMgr struct {
+	sync.RWMutex
+	defaultID string
+	dbs       map[string]*sql.DB
+}
+
+func (d *dbMgr) getDB(dbName string) (db *sql.DB, ok bool) {
+	d.RLock()
+	defer d.RUnlock()
+	db, ok = d.dbs[dbName]
+	return db, ok
+}
+
+func (d *dbMgr) setDB(dbName string, db *sql.DB) {
+	d.Lock()
+	defer d.Unlock()
+	d.dbs[dbName] = db
+}
+
+func (d *dbMgr) setDefaultID(defaultID string) {
+	d.defaultID = defaultID
+}
+
+func (d *dbMgr) getDefaultID() (defaultID string) {
+	return d.defaultID
+}
+
+func createDB(confFile string) (err error) {
+	conf, err := loadConfig(confFile)
+	if err != nil {
+		return fmt.Errorf("Load config fail, confFile = %s", confFile)
+	}
+
+	dbmgr := getDBMgrInstance()
+	dbmgr.setDefaultID(conf.DBs.Default)
+
+	for _, dbParams := range conf.DBs.DBList {
+
+		if _, ok := dbmgr.getDB(dbParams.ID); ok {
+			// DB exist no create
+			continue
+		}
+
+		di := newDBInfo()
+		if err := di.parse(dbParams.Properties); err != nil {
+			continue
+		}
+
+		if !di.check() {
+			continue
+		}
+
+		db, err := sql.Open(di.Driver, di.DataSource)
+		if err != nil {
+			log.Printf("Open database fail, err=%v, driver=%s, dbID=%s", err, di.Driver, dbParams.ID)
+			continue
+		}
+
+		err = db.Ping()
+		if err != nil {
+			log.Printf("Ping database fail, err=%v, driver=%s, dbID=%s", err, di.Driver, dbParams.ID)
+			continue
+		}
+		db.SetMaxOpenConns(di.MaxOpenConns)
+		db.SetMaxIdleConns(di.MaxIdleConns)
+
+		dbmgr.setDB(dbParams.ID, db)
+	}
+
+	return nil
+}
+
+func init() {
+	createDB("examples/gbatis.xml")
+}
