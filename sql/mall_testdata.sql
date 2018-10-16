04810b91e39065fa9ff7f3bc23d36a4476ea2902
Jack.Ju <jack.ju@itiky.com>
2018-10-16 20:34:48 +0800

    Create mall_testdata.sql

---
+++ sql/mall_testdata.sql
@@ -0,0 +1,7 @@
+use orange
+
+insert into t_mall(id, name, avatar, createdAt, updatedAt, story)
+    values('1', 'KKMall', 'img.mallbook.com/1.png', 100000, 200000, 'Hi KKMall');
+    
+insert into t_mall(id, name, avatar, createdAt, updatedAt, story)
+    values('2', 'mhMall', 'img.mallbook.com/2.png', 100000, 200000, 'Hi mhMall');
