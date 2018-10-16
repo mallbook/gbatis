155556a789e6dcc8b96e1be980a03919d44c55c9
Jack.Ju <jack.ju@itiky.com>
2018-10-16 20:22:32 +0800

    Update config.go

--- config.go
+++ config.go
@@ -2,34 +2,74 @@ package gbatis
 
 import (
 	"encoding/xml"
-	"os"
-	"log"
+	"fmt"
 	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
 )
 
 type configuration struct {
-	XMLName xml.Name     `xml:"configuration"`
-	Envs    environments `xml:"environments"`
+	XMLName xml.Name `xml:"configuration"`
+	DBs     dbs      `xml:"dbs"`
 }
 
-type environments struct {
-	Default string        `xml:"default,attr"`
-	Envs    []environment `xml:"environment"`
+type dbs struct {
+	Default string `xml:"default,attr"`
+	DBList  []db   `xml:"db"`
 }
 
-type environment struct {
+type db struct {
 	ID         string     `xml:"id,attr"`
-	DataSource dataSource `xml:"dataSource"`
-}
-
-type dataSource struct {
-	Type       string     `xml:"type,attr"`
 	Properties []property `xml:"property"`
 }
 
 type property struct {
 	Name  string `xml:"name,attr"`
 	Value string `xml:"value,attr"`
+}
+
+type dbInfo struct {
+	Driver       string
+	DataSource   string
+	MaxOpenConns int
+	MaxIdleConns int
+}
+
+func newDBInfo() *dbInfo {
+	return &dbInfo{}
+}
+
+func (di dbInfo) check() (ok bool) {
+	if di.Driver != "" && di.DataSource != "" && di.MaxOpenConns > 0 && di.MaxIdleConns > 0 && di.MaxOpenConns > di.MaxIdleConns {
+		return true
+	}
+	return false
+}
+
+func (di *dbInfo) parse(properties []property) (err error) {
+	if properties == nil {
+		return fmt.Errorf("Input parameter invalid")
+	}
+
+	for _, property := range properties {
+
+		switch property.Name {
+		case "driver":
+			di.Driver = property.Value
+			break
+		case "dataSource":
+			di.DataSource = property.Value
+			break
+		case "maxOpenConns":
+			di.MaxOpenConns, err = strconv.Atoi(property.Value)
+			break
+		case "maxIdleConns":
+			di.MaxIdleConns, err = strconv.Atoi(property.Value)
+			break
+		}
+	}
+	return nil
 }
 
 func loadConfig(configFile string) (*configuration, error) {
@@ -54,7 +94,7 @@ func loadConfig(configFile string) (*configuration, error) {
 		return nil, err
 	}
 
-	log.Println(c)
+	// log.Println(c)
 
 	return &c, nil
 }
