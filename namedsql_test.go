8bf07f443afbaeee4a19ebb37a4d74958d4bc04d
Jack.Ju <jack.ju@itiky.com>
2018-10-16 20:27:06 +0800

    Update namedsql_test.go

--- namedsql_test.go
+++ namedsql_test.go
@@ -1,15 +1,35 @@
 package gbatis
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestParseNamedSQL(t *testing.T) {
-	err := parseNamedSQL("examples/test.xml")
-	if err == nil {
-		t.Log("OK")
-	} else {
-		t.Log("error")
+func TestWalk(t *testing.T) {
+	namedFiles, err := walk("namedsql/mysql")
+	if err != nil {
 		t.Fail()
+	} else {
+		t.Log(namedFiles)
+		t.Log("OK")
 	}
+}
+
+func TestLoadNamedSQL(t *testing.T) {
+	err := loadNamedSQL("namedsql")
+	if err != nil {
+		t.Fail()
+		return
+	}
+
+	s := getSQLMgrInstance()
+	s.display()
+
+	si, err := s.getSQL("mapper.AuthorMapper.updateAuthor")
+	if err != nil {
+		t.Fail()
+		return
+	}
+
+	fmt.Printf("[%s]", formatSQL(si.sql))
 }
