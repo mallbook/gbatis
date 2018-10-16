c5cb09a7a51beb85bde2352aedcc8af99e56a9c1
Jack.Ju <jack.ju@itiky.com>
2018-10-16 20:26:27 +0800

    Update namedsql.go

--- namedsql.go
+++ namedsql.go
@@ -1,67 +1,202 @@
 package gbatis
 
 import (
-	"encoding/xml"
 	"fmt"
-	"io/ioutil"
+	"log"
 	"os"
+	"path/filepath"
+	"strings"
+	"sync"
 )
 
-type Mapper struct {
-	XMLName   xml.Name   `xml:"mapper"`
-	Namespace string     `xml:"namespace,attr"`
-	Select    []Selecter `xml:"select"`
-	Delete    []Deleter  `xml:"delete"`
-	Insert    []Inserter `xml:"insert"`
-	Update    []Updater  `xml:"update"`
+type sqlClass int8
+
+const (
+	selectClass    sqlClass = 0
+	insertClass    sqlClass = 1
+	updateClass    sqlClass = 2
+	deleteClass    sqlClass = 3
+	procStoreClass sqlClass = 4
+)
+
+// NamedSQLInfo means NamedSQL infomation
+type sqlInfor struct {
+	class      sqlClass
+	sql        string
+	resultType string
 }
 
-type Selecter struct {
-	ID  string `xml:"id,attr"`
-	SQL string `xml:",innerxml"`
+// namedSQL include all namedSQL
+// var namedSQL = make(map[string]sqlInfor)
+
+var (
+	_sqlMgrInst *sqlMgr
+	_sqlMgrOnce sync.Once
+)
+
+type sqlMgr struct {
+	sync.RWMutex
+	sqls map[string]sqlInfor
 }
 
-type Deleter struct {
-	ID  string `xml:"id,attr"`
-	SQL string `xml:",innerxml"`
+func getSQLMgrInstance() *sqlMgr {
+
+	_sqlMgrOnce.Do(func() {
+		_sqlMgrInst = &sqlMgr{
+			sqls: make(map[string]sqlInfor),
+		}
+	})
+	return _sqlMgrInst
 }
 
-type Inserter struct {
-	ID  string `xml:"id,attr"`
-	SQL string `xml:",innerxml"`
+func (s *sqlMgr) getSQL(sqlID string) (si sqlInfor, err error) {
+	s.RLock()
+	defer s.RUnlock()
+
+	si, ok := s.sqls[sqlID]
+	if !ok {
+		return si, fmt.Errorf("Not found sql named %s", sqlID)
+	}
+	return si, nil
 }
 
-type Updater struct {
-	ID  string `xml:"id,attr"`
-	SQL string `xml:",innerxml"`
+func (s *sqlMgr) appendSQL(m *mapper) {
+
+	s.Lock()
+	defer s.Unlock()
+
+	for _, item := range m.Insert {
+		infor := sqlInfor{
+			class: insertClass,
+			sql:   formatSQL(item.SQL),
+		}
+		s.sqls[m.Namespace+"."+item.ID] = infor
+	}
+	for _, item := range m.Delete {
+		infor := sqlInfor{
+			class: deleteClass,
+			sql:   formatSQL(item.SQL),
+		}
+		s.sqls[m.Namespace+"."+item.ID] = infor
+	}
+	for _, item := range m.Update {
+		infor := sqlInfor{
+			class: updateClass,
+			sql:   formatSQL(item.SQL),
+		}
+		s.sqls[m.Namespace+"."+item.ID] = infor
+	}
+	for _, item := range m.Select {
+		infor := sqlInfor{
+			class:      selectClass,
+			resultType: item.ResultType,
+			sql:        formatSQL(item.SQL),
+		}
+		s.sqls[m.Namespace+"."+item.ID] = infor
+	}
 }
 
-func parseNamedSQL(namedSQL string) error {
-	file, err := os.Open(namedSQL)
+func (s *sqlMgr) display() {
+	s.RLock()
+	defer s.RUnlock()
+
+	for id, si := range s.sqls {
+		log.Printf("sqlID=%s\n\t%s\n", id, si.sql)
+	}
+}
+
+func formatSQL(sql string) string {
+	segs := strings.Split(sql, "\n")
+	for i, seg := range segs {
+		segs[i] = strings.Trim(seg, "\t ")
+	}
+	sql = strings.Join(segs, "\n")
+	return strings.Trim(sql, "\r\n")
+}
+
+/*
+func appendNamedSQL(m *mapper) {
+
+	for _, item := range m.Insert {
+		infor := sqlInfor{
+			class: insertClass,
+			sql:   item.SQL,
+		}
+		namedSQL[m.Namespace+"."+item.ID] = infor
+	}
+	for _, item := range m.Delete {
+		infor := sqlInfor{
+			class: deleteClass,
+			sql:   item.SQL,
+		}
+		namedSQL[m.Namespace+"."+item.ID] = infor
+	}
+	for _, item := range m.Update {
+		infor := sqlInfor{
+			class: updateClass,
+			sql:   item.SQL,
+		}
+		namedSQL[m.Namespace+"."+item.ID] = infor
+	}
+	for _, item := range m.Select {
+		infor := sqlInfor{
+			class: selectClass,
+			sql:   item.SQL,
+		}
+		namedSQL[m.Namespace+"."+item.ID] = infor
+	}
+}*/
+
+/*type namedSQLMgr struct {
+	namedSQL map[string]sqlInfor
+}
+
+func (n *namedSQLMgr) (m *mapper) {
+
+}*/
+
+func walk(path string) ([]string, error) {
+	namedFiles := make([]string, 0)
+	err := filepath.Walk(path, func(path string, f os.FileInfo, err error) error {
+		if f == nil {
+			// log.Println("nil")
+			return err
+		}
+		if f.IsDir() {
+			return nil
+		}
+		// log.Println(path)
+		namedFiles = append(namedFiles, path)
+		return nil
+	})
+	return namedFiles, err
+}
+
+func loadNamedSQL(namedPath string) error {
+	namedFiles, err := walk(namedPath)
 	if err != nil {
-		fmt.Printf("error: %v", err)
+		log.Printf("Walk named sql fail, err=%s, namedPath=%s", err, namedPath)
 		return err
 	}
-	defer file.Close()
 
-	data, err := ioutil.ReadAll(file)
-	if err != nil {
-		fmt.Printf("error: %v", err)
-		return err
+	s := getSQLMgrInstance()
+
+	for index, namedFile := range namedFiles {
+		log.Println(index, namedFile)
+		m, err := parseNamedSQL(namedFile)
+		if err != nil {
+			log.Printf("Parse named sql fail, namedFile=%s, err=%s", namedFile, err)
+			continue
+		}
+		// appendNamedSQL(m)
+		s.appendSQL(m)
 	}
 
-	d := Mapper{}
-	err = xml.Unmarshal(data, &d)
-	if err != nil {
-		fmt.Printf("error: %v", err)
-		return err
-	}
-
-	fmt.Println(d)
-	fmt.Println(d.Namespace)
-	for index, value := range d.Select {
-		fmt.Printf("(%d)id=%s, sql=%s\n", index, value.ID, value.SQL)
-	}
+	// log.Println(s)
 
 	return nil
+}
+
+func init() {
+	loadNamedSQL("namedsql")
 }
