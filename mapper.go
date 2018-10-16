841ed68376630bbd54262345e2a16e50077febfe
Jack.Ju <jack.ju@itiky.com>
2018-10-16 20:24:55 +0800

    Update mapper.go

--- mapper.go
+++ mapper.go
@@ -1,20 +1,68 @@
 package gbatis
 
-type sqlAction int8
-
-const (
-	selectAction    sqlAction = 0
-	insertAction    sqlAction = 1
-	updateAction    sqlAction = 2
-	deleteAction    sqlAction = 3
-	procStoreAction sqlAction = 3
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"os"
 )
 
-// NamedSQLInfo means NamedSQL infomation
-type namedSQL struct {
-	action sqlAction
-	sql    string
+type mapper struct {
+	XMLName   xml.Name   `xml:"mapper"`
+	Namespace string     `xml:"namespace,attr"`
+	Select    []selecter `xml:"select"`
+	Delete    []deleter  `xml:"delete"`
+	Insert    []inserter `xml:"insert"`
+	Update    []updater  `xml:"update"`
 }
 
-// NamedSQL include all namedSQL
-var NamedSQL = make(map[string]namedSQL)
+type selecter struct {
+	ID         string `xml:"id,attr"`
+	ResultType string `xml:"resultType,attr"`
+	SQL        string `xml:",innerxml"`
+}
+
+type deleter struct {
+	ID  string `xml:"id,attr"`
+	SQL string `xml:",innerxml"`
+}
+
+type inserter struct {
+	ID  string `xml:"id,attr"`
+	SQL string `xml:",innerxml"`
+}
+
+type updater struct {
+	ID  string `xml:"id,attr"`
+	SQL string `xml:",innerxml"`
+}
+
+func parseNamedSQL(namedSQL string) (*mapper, error) {
+	file, err := os.Open(namedSQL)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return nil, err
+	}
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return nil, err
+	}
+
+	m := mapper{}
+	err = xml.Unmarshal(data, &m)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return nil, err
+	}
+
+	// fmt.Println(m)
+	// fmt.Println(m.Namespace)
+	// for index, value := range m.Select {
+	// 	fmt.Printf("(%d)id=%s, sql=%s\n", index, value.ID, value.SQL)
+	// }
+
+	return &m, nil
+}
