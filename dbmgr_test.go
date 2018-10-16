36074cd716b96ef6889a662535b07c5c67cb548b
Jack.Ju <jack.ju@itiky.com>
2018-10-16 20:24:26 +0800

    Create dbmgr_test.go

---
+++ dbmgr_test.go
@@ -0,0 +1,16 @@
+package gbatis
+
+import (
+	"testing"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func TestCreateDB(t *testing.T) {
+	/*err := createDB("examples/gbatis.xml")
+	if err != nil {
+		t.Fatalf("Create DB fail, err = %v", err)
+		return
+	}
+	dbmgr := getDBMgrInstance()
+	t.Log(dbmgr)*/
+}
