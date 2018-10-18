1992745a731489c6680ad6eb1a75425df1070405
Jack.Ju <jack.ju@itiky.com>
2018-10-18 20:53:37 +0800

    Update mall_testdata.sql

--- sql/mall_testdata.sql
+++ sql/mall_testdata.sql
@@ -1,7 +1,8 @@
 use orange
 
 insert into t_mall(id, name, avatar, createdAt, updatedAt, story)
-    values('1', 'KKMall', 'img.mallbook.com/1.png', 100000, 200000, 'Hi KKMall');
+    values('1', 'KKMall', 'img.mallbook.com/1.png', unix_timestamp(now()), unix_timestamp(now()), 'Hi KKMall');
     
 insert into t_mall(id, name, avatar, createdAt, updatedAt, story)
-    values('2', 'mhMall', 'img.mallbook.com/2.png', 100000, 200000, 'Hi mhMall');
+    values('2', 'mhMall', 'img.mallbook.com/2.png', unix_timestamp(now()), unix_timestamp(now()), 'Hi mhMall');
+
