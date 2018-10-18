426e98207f3694334dcee95f8ffe7f5659f7ff3e
Jack.Ju <jack.ju@itiky.com>
2018-10-18 20:49:45 +0800

    Update session_test.go

--- session_test.go
+++ session_test.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/mallbook/gbatis/uuid"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/mallbook/gbatis"
 	_ "github.com/mallbook/gbatis/bean"
@@ -27,7 +29,7 @@ func TestSelectOne(t *testing.T) {
 	}
 	defer s.Close()
 
-	row, err := s.SelectRow("mapper.mall.selectMall", "1")
+	row, err := s.SelectRow("mapper.mall.selectMall", "4")
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -54,7 +56,7 @@ func TestSelect(t *testing.T) {
 	}
 	defer s.Close()
 
-	results, err := s.Select("mapper.mall.selectMall", "1")
+	results, err := s.Select("mapper.mall.selectMall", "4")
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -79,4 +81,116 @@ func TestSelect_2(t *testing.T) {
 	}
 
 	fmt.Println(results)
+}
+
+func TestInsert_1(t *testing.T) {
+	s, err := gbatis.OpenSession()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer s.Close()
+
+	u := uuid.New()
+
+	r, err := s.Execute("mapper.mall.insertMall", u.String(), "xxmall", "mallbook.com/img/xx.png", "hi xxmall")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	rowsAffected, err := r.RowsAffected()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Insert success, rowsAffected=%d\n", rowsAffected)
+}
+
+func TestUpdate_1(t *testing.T) {
+	s, err := gbatis.OpenSession()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer s.Close()
+
+	r, err := s.Execute("mapper.mall.updateMall", "New Name", "4")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	rowsAffected, err := r.RowsAffected()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Update success, rowsAffected=%d\n", rowsAffected)
+}
+
+func TestDelete_1(t *testing.T) {
+	s, err := gbatis.OpenSession()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer s.Close()
+
+	r, err := s.Execute("mapper.mall.deleteMall", "1")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	rowsAffected, err := r.RowsAffected()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("Delete success, rowsAffected=%d\n", rowsAffected)
+}
+
+func TestBulkInsert_1(t *testing.T) {
+
+	rows := make([]interface{}, 0)
+
+	for i := 0; i < 10; i++ {
+		row := make([]interface{}, 0)
+		u := uuid.New()
+		row = append(row, u.String(), "a mall", "a.png", "hi a mall")
+		rows = append(rows, row)
+	}
+
+	s, err := gbatis.OpenSession()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer s.Close()
+
+	r, err := s.BulkInsert("mapper.mall.insertMall", rows)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	rowsAffected, err := r.RowsAffected()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("BulkInsert rows affected is %d\n", rowsAffected)
+}
+
+// func TestMain(m *testing.M) {
+func init() {
+	err := gbatis.OpenDB("examples/gbatis.xml")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Open database success.")
 }
