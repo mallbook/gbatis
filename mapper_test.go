58f9b55725fe5f48afbf3f3ae36e04c7665caf8c
Jack.Ju <jack.ju@itiky.com>
2019-04-01 21:43:57 +0800

    Update mapper_test.go

--- mapper_test.go
+++ mapper_test.go
@@ -2,14 +2,23 @@ package gbatis
 
 import (
 	"testing"
+
+	. "gopkg.in/check.v1"
 )
 
-func TestParseNamedSQL(t *testing.T) {
+func TestMapper(t *testing.T) { TestingT(t) }
+
+type MapperSuite struct{}
+
+var _ = Suite(&MapperSuite{})
+
+func (s MapperSuite) TestParseNamedSQL(c *C) {
 	mapper, err := parseNamedSQL("examples/test.xml")
-	if err == nil {
-		t.Log(mapper)
-		t.Log("OK")
-	} else {
-		t.Fail()
-	}
+	c.Assert(err, IsNil)
+	c.Check(mapper.Namespace, Equals, "/mapper/AuthorMapper")
+	c.Check(mapper.Select, HasLen, 1)
+	c.Check(mapper.Insert, HasLen, 1)
+	c.Check(mapper.Delete, HasLen, 1)
+	c.Check(mapper.Update, HasLen, 1)
+	c.Check(mapper.Anysql, HasLen, 2)
 }
