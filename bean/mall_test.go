5c766683b1aae199fb92512ad84ff47cd4c8bc58
jack.ju <jack.ju@itiky.com>
2019-04-01 23:02:13 +0800

    remove unuseful files

--- bean/mall_test.go
+++ bean/mall_test.go
@@ -1,28 +1,78 @@
 package bean
 
 import (
-	// "encoding/json"
-	"log"
-	"testing"
+	"encoding/json"
+
+	"github.com/mallbook/gbatis"
 )
 
-func TestMall(t *testing.T) {
-	m := NewMall()
-	// fmt.Println(t.(type))
-	//v := reflect.Value(m)
-	//fmt.Println(v)
-	m.ID = "1"
-	m.Name = "KKMall"
-	m.Avatar = "1.gif"
-	m.Story = "HI KKMall"
+// Mall model mall
+type Mall struct {
+	ID        string
+	Name      string
+	Avatar    string
+	CreatedAt int
+	UpdatedAt int
+	Story     string
+}
 
-	log.Print(m)
-	// json.Marshal(m)
-	// var s string
-	/*if s, err := json.Marshal(m); err == nil {
-		log.Println(string(s))
-	} else {
-		log.Println(err)
-	}*/
-	// log.Println(s)
+// String serial mall object
+func (m Mall) String() string {
+	s, err := json.Marshal(m)
+	if err != nil {
+		return err.Error()
+	}
+	return string(s)
+}
+
+// NewMall create a mall object
+func NewMall() *Mall {
+	return &Mall{}
+}
+
+// Shop model shop
+type Shop struct {
+	ID      string
+	BrandID string
+}
+
+// String serial a shop object
+func (shop Shop) String() string {
+	s, err := json.Marshal(shop)
+	if err != nil {
+		return err.Error()
+	}
+	return string(s)
+}
+
+// NewShop new a shop
+func NewShop() *Shop {
+	return &Shop{}
+}
+
+// Brand model brands
+type Brand struct {
+	ID     string
+	Name   string
+	Avatar string
+	Story  string
+}
+
+func (b Brand) String() string {
+	s, err := json.Marshal(b)
+	if err != nil {
+		return err.Error()
+	}
+	return string(s)
+}
+
+// NewBrand create a brand object
+func NewBrand() *Brand {
+	return &Brand{}
+}
+
+func init() {
+	gbatis.RegisterBean("bean.Mall", NewMall)
+	gbatis.RegisterBean("bean.Shop", NewShop)
+	gbatis.RegisterBean("bean.Brand", NewBrand)
 }
--- sql/mall.sql
+++ sql/mall.sql
@@ -1,29 +1,29 @@
 use MALL
 
 CREATE TABLE IF NOT EXISTS t_mall (
-    id varchar(64),
-    name varchar(32),
-    avatar varchar(256),
-    createdAt int,
-    updatedAt int,
+    id char(36) NOT NULL,
+    name varchar(64) NOT NULL,
+    avatar varchar(256) NOT NULL,
+    createdAt int NOT NULL,
+    updatedAt int NOT NULL,
     story varchar(1024),
     PRIMARY KEY(id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS t_shop (
-    id varchar(64),
-    brandId varchar(64),
-    createdAt int,
-    updatedAt int,
+    id char(36) NOT NULL,
+    brandId char(36) NOT NULL,
+    createdAt int NOT NULL,
+    updatedAt int NOT NULL,
     PRIMARY KEY(id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS t_brand (
-    id varchar(64),
-    name varchar(32),
-    avatar varchar(256),
-    createdAt int,
-    updatedAt int,
+    id char(36) NOT NULL,
+    name varchar(64) NOT NULL,
+    avatar varchar(256) NOT NULL,
+    createdAt int NOT NULL,
+    updatedAt int NOT NULL,
     story varchar(1024),
     PRIMARY KEY (id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
